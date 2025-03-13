af3123c980a35e3db1468f6b8e766e235f81848c
Lukas Meier <me@lmeier.ch>
2025-03-13 10:41:28 +0100

    Adds keybind to save a buffer when pressing ctrl+s

--- lua/options.lua
+++ lua/options.lua
@@ -72,3 +72,7 @@ vim.keymap.set("v", "c", '"_c', { noremap = true })
 
 -- Map to format the whole buffer
 vim.keymap.set("n", "<Leader>fa", vim.lsp.buf.format, { noremap = true, silent = true })
+
+-- Map keys to allow saving a buffer when pressing Ctrl+s (in normal aswell as in insert mode)
+vim.keymap.set("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>a", { noremap = true, silent = true })
