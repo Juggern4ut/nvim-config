98d0bd55f498e145c6baa48cd1099bd0f51ce5e1
Lukas Meier <me@lmeier.ch>
2025-03-13 10:18:17 +0100

    nvim tree now displays files hidden by .gitignore

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -2,6 +2,11 @@
 return {
     "nvim-tree/nvim-tree.lua",
     config = function()
-        require("nvim-tree").setup()
+        require("nvim-tree").setup({
+            filters = {
+                dotfiles = false,
+                git_ignored = false,
+            },
+        })
     end
 }
